e713058c30d32e1cd6c8d87089768d903a107c73
unsektor <me@unsektor.com>
2016-06-01 14:46:05 +0300

    Major update, optimizations, restructured & refactored code
    Major update, optimizations, restructured & refactored code ver 0.1.0

--- line-tools.js
+++ line-tools.js
@@ -1,161 +1,110 @@
 /**
-  *	PARSE ARGUMENTS
-  *
-  *	@author				tm.unsektor <unsektor@tm-ln.ru>
-  * @since				April 15, 2013
-  *
-  *	@description		Based on <http://pwfisher.com/2009/commandline-parseargs-php-cli-options-parser.html>
-  * 					by Patrick Fisher <patrick@pwfisher.com> @ August 21, 2009
-  *
-  *	@usage				var arguments = require('line-tools').parse(process.argv);
-  * @web-site			<https://github.com/unsektor/line-tools>
-  */
+ *	line-tools.js - NodeJS CLI Arguments parser
+ *
+ *	@author			Михаил Драгункин <contact@unsektor.com>
+ *	@ver			0.1.0
+ *	@since			April 15, 2013
+ *
+ *	@description	Based on <http://pwfisher.com/2009/commandline-parseargs-php-cli-options-parser.html>
+ * 	by Patrick Fisher <patrick@pwfisher.com> @ August 21, 2009
+ *
+ *	@usage			var arguments = require('line-tools').parse(process.argv);
+ *	@web-site 		<https://github.com/unsektor/line-tools>
+ */
 
-var strpos =	function(haystack,needle,offset)
-		{	
-				//	original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-			
-				var i = haystack.indexOf( needle, offset ); // returns -1
-				return i >= 0 ? i : false;
-		},
-	is_int	=	function is_int (mixed_var)
-		{
-			// http://kevin.vanzonneveld.net
-			// +   original by: Alex
-			// +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-			// +    revised by: Matt Bradley
-			// +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-			// +   improved by: WebDevHobo (http://webdevhobo.blogspot.com/)
-			// +   improved by: Rafał Kukawski (http://blog.kukawski.pl)
-			// %        note 1: 1.0 is simplified to 1 before it can be accessed by the function, this makes
-			// %        note 1: it different from the PHP implementation. We can't fix this unfortunately.
-			// *     example 1: is_int(23)
-			// *     returns 1: true
-			// *     example 2: is_int('23')
-			// *     returns 2: false
-			// *     example 3: is_int(23.5)
-			// *     returns 3: false
-			// *     example 4: is_int(true)
-			// *     returns 4: false
-			
-			return mixed_var === +mixed_var && isFinite(mixed_var) && !(mixed_var % 1);
-		},		
-	_ = function(key) // just debug function
-		{
-			console.log(key);process.exit();
-		};
-	
-module.exports = 
+module.exports = {
+    /**
+     *	@method parse
+     *	@param {Object} argv - process.argv object
+     *	@param {Boolean} options_only - flag
+     *	@returns {(Bool|Object)} - returns false if type of argv is not 'object', and object of arguments 
+     */
+    parse: function(argv, options_only) {
+        if (typeof(argv) != 'object') {
+            return false;
+        };
 
-new function()
-	{
-		var arguments;
-		
-		this.parse = function (argv, ready)
-			{
-				if ((typeof(argv)) != 'object') return;
-				if (!ready) argv = argv.splice(2);
-				
-				var out	= {};
-				
-				for (arg in argv)
-					{
-						var arg = argv[arg];
-						// --foo --bar=baz
-						if (arg.substr(0, 2) == '--')
-							{
-								var eqPos	=	strpos(arg, '=');
-							// --foo
-								if (eqPos === false)
-									{
-										var key		=	arg.substr(2);
-										var value	=	(typeof(out[key]) != 'undefined') ? out[key] : true;
-										out[key]	=	value;
-									}
-							// --bar=baz
-								else
-									{
-										var key		=	arg.substr(2, eqPos-2);
-										var value	=	arg.substr(eqPos+1);
-										out[key]	=	value;
-									}
-							}
-						// -k=value -abc
-						else if (arg.substr(0, 1) == '-')
-							{
-								// -k=value
-								if (arg.substr(2, 1) == '=')
-									{
-										var key		=	arg.substr(1, 1);
-										var value	=	arg.substr(3);
-										out[key]	=	value;
-									}
-								// -abc
-								else
-									{
-										var chars	=	arg.substr(1);
-										for (char in chars)
-											{
-												var char	=	chars[char];
-												var key		=	char;
-												var value	=	(typeof(out[key]) != 'undefined') ? out[key] : true;
-												out[key]	=	value;
-											}
-									}
-							}
-						// plain-arg
-						else
-							{
-								value		=	arg;
-								out[value]	=	true;
-							}
-					}
-				arguments = out;
-				
-				return out;
-			};
-		
-		this.getbool = function(value, xdefault)
-			{
-				var xdefault = !(typeof(xdefault) != 'undefined') ? false : xdefault;
-				
-				if (typeof(value) == 'boolean')
-					{
-						return value;
-					}
-				
-				if (is_int(value))
-					{
-						return (value > 0) ? true : false;
-					}
-				
-				if (typeof(value) == 'string')
-					{
-						value	= value.toLowerCase();
-						var map	=	
-							{
-								'y': 		true,
-								'n':		false,
-								
-								'yes':		true,
-								'no':		false,
-								
-								'true':		true,
-								'false':	false,
-								
-								'1':		true,
-								'0':		false,
-								
-								'on':		true,
-								'off':		false
-							};
-						
-						if (typeof(map[value]) != 'undefined')
-							{
-								return map[value];
-							}
-					}
-				
-				return xdefault;
-			}
-	}
+        if (!options_only) {
+            argv = argv.splice(2);
+        };
+
+        var key, value, result = {};
+
+        for (i in argv) {
+            if (argv[i].substr(0, 2) == '--') {
+                // --foo --bar=baz
+                var eqPos = argv[i].indexOf('=');
+
+                if (eqPos === -1) {
+                    // --foo
+                    key = argv[i].substr(2);
+                    value = (typeof(result[key]) != 'undefined') ? result[key] : true;
+                    result[key] = value;
+                } else {
+                    // --bar=baz
+                    key = argv[i].substr(2, eqPos - 2);
+                    value = argv[i].substr(eqPos + 1);
+                    result[key] = value;
+                }
+            } else if (argv[i].substr(0, 1) == '-') {
+                // -k=value -abc
+                if (argv[i].substr(2, 1) == '=') {
+                    // -k=value
+                    key = argv[i].substr(1, 1);
+                    value = argv[i].substr(3);
+                    result[key] = value;
+                } else {
+                    // -abc
+                    var chars = argv[i].substr(1);
+                    for (char in chars) {
+                        key = chars[char];
+                        value = (typeof(result[key]) != 'undefined') ? result[key] : true;
+                        result[key] = value;
+                    };
+                };
+            } else {
+                // plain-arg
+                value = argv[i];
+                result[value] = true;
+            };
+        };
+
+        return result;
+    },
+
+    /**
+     *	@method getbool
+     *	@param {String} value
+     *	@param {Mixed} defaultValue - what to return if passed value is not in special keywords list 
+     *	@returns {Boolean} - boolean value of passed value
+     */
+    getbool: function(value, defaultValue) {
+        switch (typeof(value)) {
+            case 'boolean':
+                return value;
+                break;
+
+            case 'string':
+                var keywords = {
+                    'y': true,
+                    'n': false,
+                    'yes': true,
+                    'no': false,
+                    'true': true,
+                    'false': false,
+                    '1': true,
+                    '0': false,
+                    'on': true,
+                    'off': false
+                };
+
+                value = value.toLowerCase();
+                return (typeof(keywords[value]) != 'undefined') ? keywords[value] : (!(defaultValue !== undefined) ? false : defaultValue);
+                break;
+
+            default:
+                return Boolean(value);
+                break;
+        };
+    }
+};
--- test/index.js
+++ test/index.js
@@ -1,9 +1,7 @@
-/*
+/**
  *	Run this script with arguments like:
- *	> node index.js plain-arg --foo --bar=baz --funny="spam=eggs" --alsofunny=spam=eggs 'plain arg 2' -abc -k=value "plain arg 3" --s="original" --s='overwrite' --s
+ *	$ node index.js plain-arg --foo --bar=baz --funny="spam=eggs" --alsofunny=spam=eggs 'plain arg 2' -abc -k=value "plain arg 3" --s="original" --s='overwrite' --s
  */
-
-var test	=	(process.argv.length > 2) ? process.argv : false,
-	argv	=	test || ['plain-arg', '--foo', '--bar=baz', '--funny="spam=eggs"', '--alsofunny=spam=eggs', '\'plain arg 2\'', '-abc', '-k=value', '"plain arg 3"',  '--s="original"', '--s=\'overwrite\'', '--s'];
-
-console.log(require('../line-tools').parse(argv, ((test != false) ? 0 : 1)));
+console.log(
+	require('../line-tools').parse(process.argv)
+);
